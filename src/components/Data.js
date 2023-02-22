d0504036cda4dcb79e6acda13cd30c5369bc76b6
Nikitaeloxin <nikitaeloxin@yandex.ru>
2023-02-23 00:13:16 +0200

    Add hooks

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,49 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from "./FormControled";
 import Weather from "./Weather";
 import {base_url, key} from "../utils/constants";
 
-class Data extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            weatherInfo:{
-
-            },
-            message:'Enter city name'
-        }
-    }
-    getWeather = async (city) =>{
+const Data =()=>{
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         weatherInfo:{
+    //
+    //         },
+    //         message:'Enter city name'
+    //     }
+    // }
+    const [weatherInfo,setWeatherInfo] = useState('');
+    const [message,setMessage] = useState();
+    const getWeather = async (city) =>{
         try {
             const response = await fetch(`${base_url}?q=${city}&appid=${key}&units=metric`)
             if (!response.ok){
                 throw new Error(response.status)
             }
             const data = await response.json();
-            this.setState({
-                weatherInfo: {
-                    country: data.sys.country,
-                    city:data.name,
-                    pressure:data.main.pressure,
-                    temp:data.main.temp,
-                    sunset: data.sys.sunset
-                },
-                message:null
-            })
+            const wi1 = {
+                country: data.sys.country,
+                city:data.name,
+                pressure:data.main.pressure,
+                temp:data.main.temp,
+                sunset: data.sys.sunset
+            }
+            setWeatherInfo(wi1);
+            setMessage(null)
+
         }   catch (e){
             console.log(e.message);
-            this.setState({message:'Enter correct city name'})
+            setMessage('Enter correct city name')
 
         }
 
 
     }
-    render () {
+
         return <div>
-            <Form getWeather={this.getWeather}/>
-            <Weather info={this.state.weatherInfo} message={this.state.message}/>
+            <Form getWeather={getWeather}/>
+            <Weather info={weatherInfo} message={message}/>
         </div>
-    };
 };
 
 export default Data;
